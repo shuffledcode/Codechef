bcf2418d80593614a914609bc9c48a54f851ab34
shuffledcode <79659212+shuffledcode@users.noreply.github.com>
2021-03-15 20:41:24 +0530

    Update and rename main2.py to 2-Chef and Groups.py

---
+++ 2-Chef and Groups.py
@@ -0,0 +1,1 @@
+for _ in range(int(input())): print(len([i for i in input().split('0') if i!='']))
--- main2.py
+++
@@ -1,4 +0,0 @@
-import sys
-sys.stdin = open('input.txt','r')
-sys.stdout = open('output.txt','w')
-for _ in range(int(input())): print(len([i for i in input().split('0') if i!='']))
